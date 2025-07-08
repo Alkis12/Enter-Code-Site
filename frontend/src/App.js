530bb0e5a9b603fd0c529ac289b8e9d7928b2664
Sofiia Kaikova <rainbowmilk4@gmail.com>
2025-07-08 17:07:01 +0200

    working setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <h1 style={{ border: "2px solid red", background: "yellow" }}>
+            Home Page
+          </h1>
+        }
+      />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
