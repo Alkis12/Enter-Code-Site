993793da8e7807dcd303a9e48963d0f8e2ad2d54
Sofiia Kaikova <rainbowmilk4@gmail.com>
2025-10-25 17:42:12 +0200

    my groups endpoints 1.0

--- frontend/src/pages/MyCoursesPage.js
+++ frontend/src/pages/MyCoursesPage.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Header from "../components/Header/Header";
+import { useMyGroups } from "../hooks/useMyGroups";
+import CourseWidget from "../components/MyCoursesComp/CourseWidget";
 
 function MyCourses() {
+  const { groups, loading, error } = useMyGroups();
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div style={{ color: "red" }}>{error}</div>;
+
   return (
     <div>
       <Header />
+      <CourseWidget {...groups} />
     </div>
   );
 }
